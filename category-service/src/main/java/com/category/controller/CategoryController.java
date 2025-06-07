257caadea5dca53c971164385a1de2a1be9cdcdb
Sahil Bhukal <sahil.bhukal@redblink.com>
2025-06-07 16:54:08 +0530

    Added changes for implementing category service

--- category-service/src/main/java/com/category/controller/CategoryController.java
+++ category-service/src/main/java/com/category/controller/CategoryController.java
@@ -5,10 +5,9 @@ import com.category.dto.CategoryDTO;
 import com.category.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/category")
@@ -17,10 +16,45 @@ public class CategoryController {
     @Autowired
     CategoryService categoryService ;
 
+    // this is specific for salon owner
     @PostMapping("/create")
     public ResponseEntity<Category> createCategory(@RequestBody CategoryDTO categoryDTO) {
+
+        // Get current userId -> current user salon id -> save category in that salon
+
         Category createdCategory = categoryService.createCategory(categoryDTO);
         return ResponseEntity.ok(createdCategory);
+    }
+
+    @GetMapping("/salon/{salonId}")
+    public ResponseEntity<Set<Category>> getAllBySalonId(@PathVariable Long salonId) {
+        Set<Category> categories = categoryService.getAllCategoryBySalonId(salonId);
+        return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long categoryId) {
+        try {
+            Category category = categoryService.getCategoryById(categoryId);
+            return ResponseEntity.ok(category);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // this is specific for salon owner
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<Void> deleteCategory(
+            @PathVariable Long categoryId,
+            @RequestParam Long salonId) {
+
+        try {
+            Category category = categoryService.getCategoryById(categoryId);
+            categoryService.deleteCategoryById(category, salonId);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(403).build();
+        }
     }
 
 }
--- category-service/src/main/java/com/category/service/CategoryService.java
+++ category-service/src/main/java/com/category/service/CategoryService.java
@@ -6,27 +6,37 @@ import com.category.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 public class CategoryService {
 
     @Autowired
-    CategoryRepository categoryRepository ;
+    private CategoryRepository categoryRepository;
 
     public Category createCategory(CategoryDTO categoryDTO) {
         Category category = new Category();
-
-        if (categoryDTO.getName() != null) {
-            category.setName(categoryDTO.getName());
-        }
-        if (categoryDTO.getImage() != null) {
-            category.setImage(categoryDTO.getImage());
-        }
-        if (categoryDTO.getSalonId() != null) {
-            category.setSalonId(categoryDTO.getSalonId());
-        }
+        category.setName(categoryDTO.getName());
+        category.setImage(categoryDTO.getImage());
+        category.setSalonId(categoryDTO.getSalonId());
 
         return categoryRepository.save(category);
     }
 
+    public Set<Category> getAllCategoryBySalonId(Long salonId) {
+        return categoryRepository.findBySalonId(salonId);
+    }
 
+    public Category getCategoryById(Long categoryId) throws Exception {
+        return categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new Exception("Category not found with id: " + categoryId));
+    }
+
+    public void deleteCategoryById(Category category, Long salonId) throws Exception {
+        if (!category.getSalonId().equals(salonId)) {
+            throw new Exception("You don't have permission to delete this category");
+        }
+
+        categoryRepository.deleteById(category.getId());
+    }
 }
